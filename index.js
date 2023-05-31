d1f3a23113a2907f78edca9c1d648c000f501796
gabrielapl <gabrielapl@unipam.edu.br>
2023-05-31 20:17:49 -0300

    new version expo

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-import "expo-dev-client";
+import { registerRootComponent } from 'expo';
 
-import { registerRootComponent } from "expo";
-
-import App from "./App";
+import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
-
-//"currentFullName": "@gxguinho/sun-system",
-//"originalFullName": "@gxguinho/sun-system",
--- src/components/Background/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const Container = styled.ImageBackground`
-  flex: 1;
-
-  background-color: #000;
-  padding: 34px 20px;
-`;
