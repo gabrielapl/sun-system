8d7b2a4b5e920ffad4209264c3aef0f29867abdf
gxguinho <gabrielapl@unipam.edu.br>
2023-06-05 09:32:51 -0300

    add redirect page

---
+++ app/oauthredirect.tsx
@@ -0,0 +1,5 @@
+import { Loading } from '../src/pages/loading'
+
+export default function oauthredirect() {
+  return <Loading />
+}
--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -2,8 +2,8 @@ import { View, ActivityIndicator } from 'react-native'
 
 export function Loading() {
   return (
-    <View>
-      <ActivityIndicator />
+    <View className="flex-1 items-center justify-center bg-brand_background">
+      <ActivityIndicator size={48} color="#FFF" />
     </View>
   )
 }
